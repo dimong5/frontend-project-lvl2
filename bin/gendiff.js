813f38a1439f5eb08a9f0f290a54916dfad284da
dimong5 <grygymev@mail.ru>
2020-11-01 20:03:31 +0300

    Step#2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,8 @@ const { program } = require('commander');
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1', '-V, --version', 'output the current version');
+  .version('0.0.1', '-V, --version', 'output the version number')
+  .requiredOption('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse(process.argv);
